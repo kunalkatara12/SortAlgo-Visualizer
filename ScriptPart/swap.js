6dc5468a4ac64345946fbbd23ef48bb3511cfd4d
kunalkatara12 <kunalkatara888@gmail.com>
2022-09-06 13:58:57 +0530

    changes in swap.js

--- ScriptPart/swap.js
+++ ScriptPart/swap.js
@@ -1,6 +1,8 @@
 async function swap(el1, el2, n) {
-  console.log("In swap()");
+  console.log("In swap()");  
   [el1.style.height,el2.style.height]=[el2.style.height,el1.style.height];
+  [el1.innerText,el2.innerText]=[el2.innerText,el1.innerText];
+
 }
 
 
